8adbbf47af5fcfe658347c1a8a613d564d9f8e5f
andyfaceplugin <info@faceplugin.com>
2024-10-13 20:19:14 -0700

    Hello world

---
+++ FaceRecognition-.Net/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FaceRecognition_.Net
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FaceRecognition-.Net/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FaceRecognition_.Net
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
