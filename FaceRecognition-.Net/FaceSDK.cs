4e6df84897db9e571488f10a5a9dd56452096775
andyfaceplugin <info@faceplugin.com>
2024-10-23 09:48:03 -0700

    feat: read local image and include file

---
+++ FaceRecognition-.Net/FaceSDK.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceSDK
+{
+    public class FaceEngineClass
+    {
+        public FaceEngineClass()
+        {
+
+        }
+
+        [DllImport("FacepluginSDK.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getMachineCode();
+
+        public String GetMachineCode()
+        {
+            try
+            {
+                IntPtr machineCode = getMachineCode();
+                if (machineCode == null)
+                    throw new Exception("Failed to retrieve machine code.");
+
+                string strMachineCode = Marshal.PtrToStringAnsi(machineCode);
+                return strMachineCode;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+    }
+}
--- FaceRecognition-.Net/MainPage.xaml.cs
+++ FaceRecognition-.Net/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace FaceRecognition_.Net
+﻿using FaceSDK;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System.Reflection;
+
+namespace FaceRecognition_.Net
 {
     public partial class MainPage : ContentPage
     {
@@ -19,7 +24,42 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+
         }
+
+        private void OnGetMachineCode(object sender, EventArgs e)
+        {
+            FaceEngineClass faceSDK = new FaceEngineClass();
+
+            MachineCodeLabel.Text = faceSDK.GetMachineCode();
+            SemanticScreenReader.Announce(MachineCodeLabel.Text);
+
+            ImageCaption.Source = ImageSource.FromFile("dotnet_bot.png");
+
+            // Read local image, convert it to byte array, and get image size
+            string imagePath = "E:/1.jpg";
+            byte[] imageBytes;
+            int imageWidth, imageHeight;
+
+            using (var stream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    imageBytes = memoryStream.ToArray();
+                }
+            }
+
+            // Get image size
+            using (var image = System.Drawing.Image.FromFile(imagePath))
+            {
+                imageWidth = image.Width;
+                imageHeight = image.Height;
+            }
+
+        }
+
+
     }
 
 }
