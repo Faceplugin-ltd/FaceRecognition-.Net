865cc5be6ed40bebf063f30adda9ca8b6786728a
andyfaceplugin <info@faceplugin.com>
2024-10-24 17:19:42 -0700

    feat: basic UI

--- FaceRecognition-.Net/FaceSDK.cs
+++ FaceRecognition-.Net/FaceSDK.cs
@@ -7,6 +7,33 @@ using System.Threading.Tasks;
 
 namespace FaceSDK
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ResultBox
+    {
+        public int x1, y1, x2, y2;
+
+        public float yaw, roll, pitch;
+        public float face_quality, face_luminance, eye_dist;
+
+        public float left_eye_closed, right_eye_closed, face_occlusion, mouth_opened;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 68 * 2)]
+        public float[] landmark_68; // Array of 136 floats
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2048)]
+        public byte[] templates; // Array of 2048 bytes
+
+        public ResultBox(int n)
+        {
+            x1 = x2 = y1 = y2 = 0;
+            yaw = roll = pitch = 0;
+            face_quality = face_luminance = eye_dist = 0;
+            left_eye_closed = right_eye_closed = face_occlusion = mouth_opened = 0;
+            templates = new byte[2056];
+            landmark_68 = new float[68 * 2];
+        }
+    };
+
     public class FaceEngineClass
     {
         public FaceEngineClass()
@@ -15,13 +42,13 @@ namespace FaceSDK
         }
 
         [DllImport("FacepluginSDK.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr getMachineCode();
+        public static extern IntPtr Faceplugin_get_hardware_id();
 
-        public String GetMachineCode()
+        public String GetHardwareId()
         {
             try
             {
-                IntPtr machineCode = getMachineCode();
+                IntPtr machineCode = Faceplugin_get_hardware_id();
                 if (machineCode == null)
                     throw new Exception("Failed to retrieve machine code.");
 
@@ -31,6 +58,40 @@ namespace FaceSDK
             catch (Exception e)
             {
                 return e.ToString();
+            }
+        }
+
+        [DllImport("FacepluginSDK.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Faceplugin_init(IntPtr modelPath);
+
+        public int Init(string modelPath)
+        {
+            return Faceplugin_init(Marshal.StringToHGlobalAnsi(modelPath));
+        }
+
+        [DllImport("FacepluginSDK.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Faceplugin_detect(
+            IntPtr rgbData, // Pointer to the RGB data
+            int width,      // Width of the image
+            int height,     // Height of the image
+            int stride,     // Stride of the image
+            [In, Out] ResultBox[] resultBoxes, // Array of ResultBox
+            int maxCount // Number of face boxes
+        );
+
+        public int DetectFace(byte[] rgbData, int width, int height, int stride, [In, Out] ResultBox[] faceBoxes, int faceBoxCount)
+        {
+            IntPtr imgPtr = Marshal.AllocHGlobal(rgbData.Length);
+            Marshal.Copy(rgbData, 0, imgPtr, rgbData.Length);
+
+            try
+            {
+                int ret = Faceplugin_detect(imgPtr, width, height, stride, faceBoxes, faceBoxCount);
+                return ret;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(imgPtr);
             }
         }
 
--- FaceRecognition-.Net/MainPage.xaml.cs
+++ FaceRecognition-.Net/MainPage.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using FaceSDK;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace FaceRecognition_.Net
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        FaceEngineClass faceSDK = new FaceEngineClass();
 
         public MainPage()
         {
             InitializeComponent();
+
+            int ret = faceSDK.Init("E:/assets");
+
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -29,34 +37,112 @@ namespace FaceRecognition_.Net
 
         private void OnGetMachineCode(object sender, EventArgs e)
         {
-            FaceEngineClass faceSDK = new FaceEngineClass();
 
-            MachineCodeLabel.Text = faceSDK.GetMachineCode();
+            MachineCodeLabel.Text = faceSDK.GetHardwareId();
             SemanticScreenReader.Announce(MachineCodeLabel.Text);
 
-            ImageCaption.Source = ImageSource.FromFile("dotnet_bot.png");
+            //ImageCaption.Source = ImageSource.FromFile("dotnet_bot.png");
 
             // Read local image, convert it to byte array, and get image size
             string imagePath = "E:/1.jpg";
-            byte[] imageBytes;
-            int imageWidth, imageHeight;
 
-            using (var stream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            System.Drawing.Image image = null;
+            try
             {
-                using (var memoryStream = new MemoryStream())
+                image = LoadImageWithExif(imagePath);
+            }
+            catch (Exception)
+            {
+                DisplayAlert("test", "Unknown Format!", "ok");
+                return;
+            }
+
+            //byte[] imgbuf = ImageToByteArray(image);
+
+            Bitmap imgBmp = ConvertTo24bpp(image);
+            BitmapData bitmapData = imgBmp.LockBits(new Rectangle(0, 0, imgBmp.Width, imgBmp.Height), ImageLockMode.ReadWrite, imgBmp.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(imgBmp.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * imgBmp.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+
+            imgBmp.UnlockBits(bitmapData);
+
+            ResultBox[] resultBoxes = new ResultBox[10];
+            int faceCount = faceSDK.DetectFace(pixels, imgBmp.Width, imgBmp.Height, bitmapData.Stride, resultBoxes, 10);
+
+        }
+
+        public byte[] BitmapToJpegByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Save the Bitmap as JPEG to the MemoryStream
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                // Return the byte array
+                return memoryStream.ToArray();
+            }
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        public static Bitmap ConvertTo24bpp(System.Drawing.Image img)
+        {
+            var bmp = new Bitmap(img.Width, img.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            using (var gr = Graphics.FromImage(bmp))
+                gr.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
+            return bmp;
+        }
+
+        private static System.Drawing.Image LoadImageWithExif(String filePath)
+        {
+            try
+            {
+                System.Drawing.Image image = System.Drawing.Image.FromFile(filePath);
+
+                // Check if the image has EXIF orientation data
+                if (image.PropertyIdList.Contains(0x0112))
                 {
-                    stream.CopyTo(memoryStream);
-                    imageBytes = memoryStream.ToArray();
+                    int orientation = image.GetPropertyItem(0x0112).Value[0];
+
+                    switch (orientation)
+                    {
+                        case 1:
+                            // Normal
+                            break;
+                        case 3:
+                            // Rotate 180
+                            image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                            break;
+                        case 6:
+                            // Rotate 90
+                            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            break;
+                        case 8:
+                            // Rotate 270
+                            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            break;
+                        default:
+                            // Do nothing
+                            break;
+                    }
                 }
-            }
 
-            // Get image size
-            using (var image = System.Drawing.Image.FromFile(imagePath))
+                return image;
+            }
+            catch (Exception e)
             {
-                imageWidth = image.Width;
-                imageHeight = image.Height;
+                throw new Exception("Image null!");
             }
-
         }
 
 
